36d1dce9867e31f3a4134be9158694c0740726e0
yamini <vundelayamini@gmail.com>
2021-02-14 18:03:44 +0530

    Added:21timesWon

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,14 +4,16 @@ isHeads=1
 headCount=0
 tailCount=0
 
-for((count=1; count<=2; count++))  
+for((count=1; count<=100; count++))  
  do
-    flipCoin=$((RANDOM%2))
-     if [ $flipCoin -eq $isHeads ]
-then
-    headCount=$(($headCount+1))
-else
-    tailCount=$(($tailCont+1))
-fi
+    if [ $headCountWin < 21 && $tailCount< 21 ]
+     then
+          flipCoin=$((RANDOM%2))
+         if [ $flipCoin -eq $isHeads ]
+   then
+    headCount=$(($headCount++))
+  else
+    tailCount=$(($tailCount++))
+  fi
 done
 echo $headCount $tailCount
